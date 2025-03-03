4caaa431a3301cc12440c3cc70b4de85c870354b
Gian Lucca Panzone <gian.lucca1964@gmail.com>
2025-03-03 18:04:31 -0300

    fixing querys

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+from auth import ACCESS_TOKEN_EXPIRE_MINUTES, get_current_user, hash_password, oauth2_scheme
+from datetime import datetime, timedelta
 from fastapi import Depends, FastAPI, HTTPException
-from auth import get_current_user
 from pydantic import BaseModel
 from typing import List
 import sqlite3
 from fastapi import FastAPI
-from auth import router as auth_router  # Importando o router do auth.py
-from fastapi import FastAPI
-from auth import router as auth_router  # Importando o router do auth.py
+from auth import router as auth_router 
 
 app = FastAPI()
 
@@ -27,7 +26,16 @@ CREATE TABLE IF NOT EXISTS transactions (
     date TEXT NOT NULL
     )
 ''')
+conn.commit()
 
+# Criação da tabela de tokens revogados
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS token_blacklist(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    token TEXT NOT NULL,
+    expiry TIMESTAMP NOT NULL
+    )
+''')
 conn.commit()
 
 #Modelo de dados
@@ -50,7 +58,7 @@ def create_transaction(transaction: Transaction, user: str = Depends(get_current
 # Listar todas as transações
 @app.get('/transactions/', response_model = List[Transaction])
 def get_transactions():
-    cursor.execute('SELECT description, amount, tyupe, date FROM transactions')
+    cursor.execute('SELECT description, amount, type, date FROM transactions')
     transactions = cursor.fetchall()
     return [{"description": t[0], "amount": t[1], "type": t[2], "date": t[3]} for t in transactions]
 
@@ -60,6 +68,26 @@ def delete_transaction(transaction_id: int):
     cursor.execute('DELETE FROM transactions WHERE id = ?', (transaction_id,))
     conn.commit()
     return {'message': 'Transação deletada com sucesso'}
+
+class UpdateUser(BaseModel):
+    username: str
+    password: str
+
+# Rota de atualização de Usuário
+@app.put('/users/update/')
+def update_user(user: UpdateUser, token: str = Depends(oauth2_scheme)):
+    cursor.execute('UPDATE users SET password = ? WHERE username = ?',
+                   (hash_password(user.password), user.username))
+    conn.commit()
+    return {'message': 'Senha atualizado com sucesso'}
+
+# Rota de logout 
+@app.post("/logout/")
+def logout(token: str = Depends(oauth2_scheme)):
+    expire_time = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    cursor.execute("INSERT INTO token_blacklist (token, expiry) VALUES (?, ?)", (token, expire_time))
+    conn.commit()
+    return {"message": "Logout realizado com sucesso"}
 
 # Iniciando servidor
 if __name__ == '__main__':
